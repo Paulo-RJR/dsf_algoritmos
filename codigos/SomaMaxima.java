a6320abc9b3b1a019e7a1fdf5d7324b07c526282
Paulo Roberto J. Rodrigues <paulo-rjr11@hotmail.com>
2024-03-09 17:50:34 -0300

    Deixando o algoritmo mais limpo e simples.

--- codigos/SomaMaxima.java
+++ codigos/SomaMaxima.java
@@ -1,58 +1,31 @@
 import java.util.Scanner;
 public class SomaMaxima {
-    // Aplicando os modificadores de acesso.
-
+    // Soma Máxima com o uso da Biblioteca Math.
     private int [] vetor = new int[9];
-    private int somaSub1;
-    private int somaSub2;
-    private int somaSub3;
-    // Adicionando o método construtor padrão.
+    private int maiorValorAtual = vetor[0];
+    private int maiorValorFinal = vetor[0];
     public SomaMaxima() {
     }
+    // Método criado para resolução deste desafio.
     public void encontrarSomaMaxima(){
         Scanner entrada = new Scanner(System.in);
-        //int somaSub1, somaSub2, somaSub3;
         // Para que o usuário crie o seu Vetor de números.
         System.out.println("\nDigite os nove números para montar o seu Vetor: \n");
         for (int i = 0; i < vetor.length; i++){
             System.out.printf("Digite o " + (i+1) + "º número: ");
             vetor[i] = entrada.nextInt();
         }
-
         // Mostra o Vetor criado pelo usuário.
         System.out.print("\nVetor: [ ");
         for (int i = 0; i < vetor.length; i++){
             System.out.printf(vetor[i] + " ");
         }
         System.out.print("]\n");
-
-        // Realiza a soma dos Subvetores (com quatro elementos) do Vetor criado.
-        somaSub1 = vetor[0] + vetor[1] + vetor[2] + vetor[3];
-        // Mostra o resultado desta soma.
-        //System.out.println("\nSoma: " + somaSub1);
-
-        somaSub2 = vetor[3] + vetor[4] + vetor[5] + vetor[6];
-        // Mostra o resultado desta soma.
-        //System.out.println("\nSoma: " + somaSub2);
-
-        somaSub3 = vetor[5] + vetor[6] + vetor[7] + vetor[8];
-        // Mostra o resultado desta soma.
-        //System.out.println("\nSoma: " + somaSub3);
-        System.out.println();
-
-        // Para mostrar o Subvetor com o maior resultado da soma.
-        if (somaSub1 > somaSub2 && somaSub1 > somaSub3){
-            System.out.println("Vetor: [ " +  vetor[0] + " " + vetor[1] + " " + vetor[2] + " " + vetor[3] + " ]" );
-            System.out.println("Soma Máxima: " + somaSub1);
+        for (int i = 0; i < vetor.length; i++){
+            maiorValorAtual = Math.max(vetor[i], maiorValorAtual + vetor[i]);
+            maiorValorFinal = Math.max(maiorValorFinal, maiorValorAtual);
         }
-        if (somaSub2 > somaSub1 && somaSub2 > somaSub3){
-            System.out.println("Vetor: [ " +  vetor[3] + " " + vetor[4] + " " + vetor[5] + " " + vetor[6] + " ]" );
-            System.out.println("Soma Máxima: " + somaSub2);
-        }
-        if (somaSub3 > somaSub2 && somaSub3 > somaSub1){
-            System.out.println("Vetor: [ " + vetor[5] + " " + vetor[6] + " " + vetor[7] + " " + vetor[8] + " ]" );
-            System.out.println("Soma Máxima: " + somaSub3);
-        }
+        System.out.println("Soma Máxima: " + maiorValorFinal);
         entrada.close();
     }
 }
